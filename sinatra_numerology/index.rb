2d35820ed40a0b9d14bb3ee09e6c2aee451c447c
minnirupp <rupp77@gmail.com>
2015-08-12 20:44:14 -0500

    updated numerology with form

--- sinatra_numerology/index.rb
+++ sinatra_numerology/index.rb
@@ -44,8 +44,21 @@ get '/newpage/' do
 end
 
 get '/:birthdate' do
-birthdate = params[:birthdate]
-birth_path_num = get_birth_path_num(birthdate)
-@message = get_message(birth_path_num)
-erb :index
+	setup_index_view
+end
+
+get '/' do
+	erb :form
+end
+
+post '/' do
+	birthdate = params[:birthdate].gsub("-", "")
+	birth_path_num = get_birth_path_num(params[:birthdate])
+	redirect "/message/#{birth_path_num}"
+end
+
+get '/message/:birth_path_num' do
+	birth_path_num = params[:birth_path_num].to_i
+	@message = get_message(birth_path_num)
+	erb :index
 end
